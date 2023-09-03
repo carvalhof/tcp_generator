5ee5a143354a3cca6ef6678fed2a392a89b3e078
carvalhof <carvalhof.br@gmail.com>
2023-09-03 13:22:38 -0400

    wip

--- process_experiments.sh
+++ process_experiments.sh
@@ -3,9 +3,11 @@
 PERCENTILE_1="50.0"
 PERCENTILE_2="99.9"
 
-SERVER_LAYOUT_LIST=("l1" "l2" "l3" "l4")
-CLIENT_RATE_INITIAL=50000
-CLIENT_RATE_INCREMENTAL=50000
+SERVER_LAYOUT_LIST=("l1" "l2" "l2+" "l3" "l4" "l4+")
+CLIENT_RATE_INITIAL=10000
+CLIENT_RATE_INCREMENTAL=10000
+
+RESDIR="results-sqrt-exp-50us"
 
 error () {
     local Z=1.96
@@ -16,27 +18,35 @@ error () {
     ERROR=`awk 'BEGIN {printf "%f", '$Z' * '$STDEV'/sqrt('$N')}'`
 }
 
+percentile () {
+    local N=`wc -l $1 | cut -d' ' -f1`
+    echo $N > .tmp
+    cat $1 | cut -d$'\t' -f1 >> .tmp
+    ./percentile $2 .tmp >> $3/percentiles_$2.txt
+}
+
 for l in ${SERVER_LAYOUT_LIST[@]}; do
     CLIENT_CURRENT_RATE=$(( CLIENT_RATE_INITIAL + CLIENT_RATE_INCREMENTAL ))
-    DIR="results/$l/${CLIENT_CURRENT_RATE}"
+    DIR="$RESDIR/$l/${CLIENT_CURRENT_RATE}"
 
-    OUTPUT_FILE="results/$l.dat"
+    OUTPUT_FILE="$RESDIR/$l.dat"
     rm -rf $OUTPUT_FILE 1>/dev/null 2>/dev/null
 
     while [ -d $DIR ]; do
-        FILE1="$DIR/percentiles_${PERCENTILE_1}.txt"
-        FILE2="$DIR/percentiles_${PERCENTILE_2}.txt"
+        INPUT="$DIR/output0.dat"
 
-        if [ ! -f $FILE1 ]; then
+        if [ ! -f $INPUT ]; then
             break;
         fi
 
-        error $FILE1
+        percentile $INPUT ${PERCENTILE_1} $DIR
+        error "$DIR/percentiles_${PERCENTILE_1}.txt"
         echo -ne "$CLIENT_CURRENT_RATE\t$MEAN\t$ERROR\t" >> $OUTPUT_FILE
-        error $FILE2
+        percentile $INPUT ${PERCENTILE_2} $DIR
+        error "$DIR/percentiles_${PERCENTILE_2}.txt"
         echo -ne "$MEAN\t$ERROR\n" >> $OUTPUT_FILE
 
-	CLIENT_CURRENT_RATE=$(( CLIENT_CURRENT_RATE + CLIENT_RATE_INCREMENTAL  ))
-    DIR="results/$l/${CLIENT_CURRENT_RATE}"
+        CLIENT_CURRENT_RATE=$(( CLIENT_CURRENT_RATE + CLIENT_RATE_INCREMENTAL  ))
+        DIR="$RESDIR/$l/${CLIENT_CURRENT_RATE}"
     done
 done
